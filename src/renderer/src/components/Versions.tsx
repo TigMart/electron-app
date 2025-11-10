29e3e0a95bc5c7e730030119e43412b8909f5bde
TigMart <tig.mar@yandex.ru>
2025-11-10 15:35:46 +0400

    test

--- src/renderer/src/components/Versions.tsx
+++ src/renderer/src/components/Versions.tsx
@@ -7,7 +7,7 @@ function Versions(): React.JSX.Element {
     <ul className="versions">
       <li className="electron-version">Electron v{versions.electron}</li>
       <li className="chrome-version">Chromium v{versions.chrome}</li>
-      <li className="node-version">Node v{versions.node}</li>
+      <li className="node-version">Node {versions.node}</li>
     </ul>
   )
 }
